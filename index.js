ee9ed307d53e57b24e86b9772856d00350e13121
villysiu <villysiu@gmail.com>
2021-06-08 11:02:09 -0700

    commit tree 4cc47160bb64fc711e2a2932104125f056de97f6

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(firstLetterUpperCase)
+}
+
+function firstLetterUpperCase(string){
+  return string.split(" ").map(word => word[0].toUpperCase()+word.slice(1)).join(" ")
 }
